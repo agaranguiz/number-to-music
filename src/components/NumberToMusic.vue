ea808c80f6133a9435f537389f98f4855d58063b
Angel Aranguiz <angel.aranguiz@gmail.com>
2022-07-07 14:54:46 -0400

    Basic Vue project structure created

---
+++ src/components/NumberToMusic.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    // methods can be called in lifecycle hooks, or other methods!
+    this.increment()
+  }
+}
+</script>
+<template>
+<h1> {{count}} </h1>
+</template>
+<style>
+
+</style>
